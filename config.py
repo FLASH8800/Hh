c64a3fd835a61e8f0e25266a1370d91ba4effa1f
FLASH8800 <hop228813@gmail.com>
2024-07-29 03:16:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6809058656:AAHHOuFyIUoIDHpipU0mEUiLcVvoph7p-mE")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("FLASH_502")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1808161060")
+
+
+
+
+
+
+
+  
