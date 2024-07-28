764bbeca7d3a7da044ac0dd7891958b65e0bd9bf
FLASH8800 <hop228813@gmail.com>
2024-07-29 00:00:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7269639946:AAE2vEUJK05RBmV37nP0jOlUv0klUO3FVKY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Kalua_786")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1808161060")
+
+
+
+
+
+
+
+  
